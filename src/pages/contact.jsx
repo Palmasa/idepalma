0860711b4ba35949d20fc4f7bb2b228ee8ec7f01
Ines de Palma <ipalmamasaveu@gmail.com>
2021-08-04 12:45:35 +0200

    form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -6,7 +6,30 @@ const contact = () => {
   return (
     <Layout>
       <div className="Contact">
-        <h1>Contact</h1>
+        <div className="Contact__color">
+          <p>Inés de Palma Masaveu</p>
+          <p>ipalmamasaveu@gmail.com</p>
+          <form name="contact" method="POST" data-netlify="true" action="/projects">
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>   
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Your Role: <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        </div>
       </div>
     </Layout>
   )

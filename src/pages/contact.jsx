a18ce4ce23557d65b6b6942ad64c77acc2cd00cb
Ines de Palma <ipalmamasaveu@gmail.com>
2021-08-04 13:21:03 +0200

    forms3

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -12,19 +12,16 @@ const contact = () => {
 
           <form name="contact" method="POST" data-netlify="true">
             <p>
-              <label>Your Name: <input type="text" name="name" /></label>   
+              <label>Your Name: </label>   
+              <input type="text" name="name" />
             </p>
             <p>
-              <label>Your Email: <input type="email" name="email" /></label>
+              <label>Your Email: </label>
+              <input type="email" name="email" />
             </p>
             <p>
-              <label>Your Role: <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-              </select></label>
-            </p>
-            <p>
-              <label>Message: <textarea name="message"></textarea></label>
+              <label>Message: </label>
+              <textarea name="message"></textarea>
             </p>
             <p>
               <button type="submit">Send</button>
